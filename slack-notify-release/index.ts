462f74276a1bb66e9a8a8805c9afca4088b228ed
Mihkel Sokk <mihkelsokk@gmail.com>
2020-01-30 13:38:06 +0200

    Initial commit

---
+++ slack-notify-release/index.ts
@@ -0,0 +1,62 @@
+import { setFailed, getInput } from '@actions/core';
+import { context, GitHub } from '@actions/github';
+import { WebClient, ActionsBlock, SectionBlock, ContextBlock } from '@slack/web-api';
+import template from 'lodash.template';
+
+const now = Math.round(Date.now() / 1000);
+
+async function run(): Promise<void> {
+  try {
+    const { sha, payload } = context;
+    const isProdRelease = context.eventName === 'release';
+    const environment = isProdRelease ? 'production' : 'staging';
+    const { owner, repo } = context.repo;
+
+    const octokit = new GitHub(getInput('repo-token'));
+    const commit = await octokit.repos.getCommit({ owner, repo, ref: sha });
+    const commitLink = `<${commit.data.html_url}|${payload.repository?.name}@${sha.substring(0, 7)}> - ${
+      commit.data.commit.message
+    }`;
+    const compiledTemplate = template(getInput('template'));
+
+    let blocks: (SectionBlock | ActionsBlock | ContextBlock)[] = [
+      {
+        type: 'section',
+        text: { type: 'mrkdwn', text: `${commitLink}\n${compiledTemplate({ payload, context, environment })}` },
+      },
+      {
+        type: 'context',
+        elements: payload.release
+          ? [
+              { type: 'image', image_url: payload.release.author.avatar_url, alt_text: 'Avatar' },
+              { type: 'mrkdwn', text: `*<${payload.release.author.html_url}|${payload.release.author.login}>*` },
+            ]
+          : [{ type: 'mrkdwn', text: ':robot_face: *Github Actions*' }],
+      },
+    ];
+
+    if (isProdRelease === false) {
+      blocks.push({
+        type: 'actions',
+        elements: [
+          {
+            type: 'button',
+            text: { type: 'plain_text', text: 'Create production release' },
+            style: 'primary',
+            url: `https://github.com/${payload.repository?.full_name}/releases/new?tag=production-${now}&target=${sha}`,
+          },
+        ],
+      });
+    }
+
+    const slackToken = getInput('token');
+    const slack = new WebClient(slackToken);
+    const channel = getInput('channel');
+    await slack.chat.postMessage({ channel, blocks, text: 'Release', username: 'Release', icon_emoji: ':rocket:' });
+    console.log(`Notified to ${channel}`);
+  } catch (error) {
+    setFailed(error.message);
+  }
+}
+
+run();
